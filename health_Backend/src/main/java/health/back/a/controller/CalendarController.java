a753d1ead19879ae434249db92687b4ebbeed8f3
baewoobin <b.w.bin77@gmail.com>
2022-04-11 16:15:23 +0900

    커밋

--- health_Backend/src/main/java/health/back/a/controller/CalendarController.java
+++ health_Backend/src/main/java/health/back/a/controller/CalendarController.java
@@ -43,9 +43,18 @@ public class CalendarController {
 		}
 		return "NO";
 }
-	
+	@RequestMapping(value = "/updateCalendar_M", method = {RequestMethod.GET, RequestMethod.POST} )
+	public String updateCalendar_M(@RequestBody CalendarDto dto) {
+		System.out.println("CalendarController updateCalendar_M " + new Date());
+		
+		boolean b = service.updateCalendar(dto);
+		if(b) {
+			return "OK";
+		}
+		return "NO";
+	}
 	@RequestMapping(value = "/updateCalendar", method = {RequestMethod.GET, RequestMethod.POST} )
-	public String updateCalendar(@RequestBody CalendarDto dto) {
+	public String updateCalendar(CalendarDto dto) {
 		System.out.println("CalendarController updateCalendar " + new Date());
 		
 		boolean b = service.updateCalendar(dto);
@@ -55,8 +64,17 @@ public class CalendarController {
 		return "NO";
 	}
 	
+	@RequestMapping(value = "/deleteCalendar_M", method = {RequestMethod.GET, RequestMethod.POST} )
+	public String deleteCalendar_M(@RequestBody CalendarDto dto) {
+		System.out.println("CalendarController deleteCalendar_M " + new Date());
+		boolean result = service.deleteCalendar(dto);		
+		if(result) {
+			return "OK";
+		}
+		return "NO";	
+	}
 	@RequestMapping(value = "/deleteCalendar", method = {RequestMethod.GET, RequestMethod.POST} )
-	public String deleteCalendar(@RequestBody CalendarDto dto) {
+	public String deleteCalendar(CalendarDto dto) {
 		System.out.println("CalendarController deleteCalendar " + new Date());
 		boolean result = service.deleteCalendar(dto);		
 		if(result) {
@@ -65,17 +83,40 @@ public class CalendarController {
 		return "NO";	
 	}
 	//안드로이드에서 @RequestBody 는 객체여서 사용해야됨.
-	@RequestMapping(value = "/searchCalendar", method = {RequestMethod.GET, RequestMethod.POST} )
-	public CalendarDto searchCalendar(@RequestBody CalendarDto dto) {
-		System.out.println("CalendarController searchCalendar " + new Date());
+	@RequestMapping(value = "/searchCalendar_M", method = {RequestMethod.GET, RequestMethod.POST} )
+	public CalendarDto searchCalendar_M(@RequestBody CalendarDto dto) {
+		System.out.println("CalendarController searchCalendar_M " + new Date());
 		
-		return service.searchCalendar(dto);
-		
+		return service.searchCalendar(dto);		
 	}
 	
+	@RequestMapping(value = "/searchCalendar", method = {RequestMethod.GET, RequestMethod.POST} )
+	public CalendarDto searchCalendar( CalendarDto dto) {
+		System.out.println("CalendarController searchCalendar " + new Date());
+		
+		return service.searchCalendar(dto);		
+	}
+
 	// 기간검색
 	@RequestMapping(value = "/psearchCalendar", method = {RequestMethod.GET, RequestMethod.POST} )
 	public List<CalendarDto> psearchCalendar(int startDate, int endDate, String userId){
+		System.out.println("시작기간 : " + startDate);
+		System.out.println("종료기간 : " + endDate);
+		
+		List<CalendarDto> list = service.psearchCalendar(userId);
+		List<CalendarDto> calList = new ArrayList<CalendarDto>();
+		
+		for(int i = 0; i <list.size(); i++) {
+			if(Integer.parseInt(list.get(i).getCalendardate()) >= startDate && Integer.parseInt(list.get(i).getCalendardate()) <= endDate) {
+				calList.add(list.get(i));
+			}
+		}
+		System.out.println(calList);
+		
+		return calList;
+	}
+	@RequestMapping(value = "/psearchCalendar_M", method = {RequestMethod.GET, RequestMethod.POST} )
+	public List<CalendarDto> psearchCalendar_M(int startDate, int endDate, String userId){
 		System.out.println("시작기간 : " + startDate);
 		System.out.println("종료기간 : " + endDate);
 		
