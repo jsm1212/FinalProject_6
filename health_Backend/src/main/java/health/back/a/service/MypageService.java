0bcc71bfe298973491a7e13bf401b803d7284571
HONG <dud2680@gmail.com>
2022-04-11 16:20:31 +0900

    마이페이지/관리자페이지/메일보내기

--- health_Backend/src/main/java/health/back/a/service/MypageService.java
+++ health_Backend/src/main/java/health/back/a/service/MypageService.java
@@ -17,10 +17,27 @@ public class MypageService {
 	@Autowired
 	MypageDao dao;
 	
+	// 회원정보 호출
+	public LoginMemberDto getInformation(String id) {
+		return dao.getInformation(id);
+	}
+	
 	// 회원정보 수정
 	public boolean updateMember(LoginMemberDto dto) {
 		int i = dao.updateMember(dto);
 		return i>0 ? true:false;
+	}
+	
+	// 비밀번호 수정
+	public boolean updatePwd(LoginMemberDto dto) {
+		int i = dao.updatePwd(dto);
+		return i>0 ? true:false;
+	}
+	
+	// 회원 삭제
+	public boolean deleteMember(String id) {
+		int i = dao.deleteMember(id);
+		return i>0? true:false;
 	}
 	
 	// 운동 루틴
@@ -41,5 +58,10 @@ public class MypageService {
 	// 좋아요 누른 글 목록
 	public List<WorkBbsDto> getMyLike(String id){
 		return dao.getMyLike(id);
+	}
+	
+	// 회원 목록 조회(관리자)
+	public List<LoginMemberDto> getMemberList(){
+		return dao.getMemberList();
 	}
 }
